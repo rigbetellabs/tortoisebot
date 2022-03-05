a221d20c18fa0444b24041cf30598b1d7f12aa92
Pranshu Tople <pranshutople@gmail.com>
2022-03-06 00:38:29 +0530

    Update odom_pub.py

--- tortoisebot_firmware/scripts/odom_pub.py
+++ tortoisebot_firmware/scripts/odom_pub.py
@@ -1,4 +1,4 @@
-#!/usr/bin/ python
+#!/usr/bin/env python
 
 # This scripts takes the odom from TF published by Cartographer and publishes it as an individual topic. Only required when used with real robot.
 
