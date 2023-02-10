adcfe2df4c13ee71651de3a22ac8ded7bf3778f1
ShubhamNandi <shubhamnandi12@gmail.com>
2023-02-10 18:27:45 +0530

    updated bringup.launch.py with state publishers, upated rviz.launch.py

--- tortoisebot_bringup/launch/bringup.launch.py
+++ tortoisebot_bringup/launch/bringup.launch.py
@@ -2,23 +2,33 @@ import os
 import launch
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration, PythonExpression, Command
 from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable,IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
+import launch_ros
+from launch_ros.descriptions import ParameterValue
+
 
 def generate_launch_description():
-  
-  rviz_launch_dir=os.path.join(get_package_share_directory('tortoisebot_description'), 'launch')
+  pkg_share = launch_ros.substitutions.FindPackageShare(package='tortoisebot_description').find('tortoisebot_description')
+  default_model_path = os.path.join(pkg_share, 'models/urdf/tortoisebot.xacro')
   ydlidar_launch_dir=os.path.join(get_package_share_directory('ydlidar'), 'launch')
   use_sim_time=LaunchConfiguration('use_sim_time')
-  
-  rviz_launch_cmd=IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(rviz_launch_dir, 'rviz.launch.py')),
-            launch_arguments={'use_sim_time':use_sim_time}.items())
 
+  robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time},{'robot_description': ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),value_type=str)}]
+    )
+
+  joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters= [{'use_sim_time': use_sim_time}],
+    )
 
   ydlidar_launch_cmd=IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -36,18 +46,23 @@ def generate_launch_description():
       executable='raspicam2_node',
       name ='pi_camera',
     )
-    #imu
+
   return LaunchDescription([
+    launch.actions.DeclareLaunchArgument(
+      name='model', 
+      default_value=default_model_path,
+      description='Absolute path to robot urdf file'),
     launch.actions.
     DeclareLaunchArgument(
       name='use_sim_time', 
       default_value='False',
       description='Flag to enable use_sim_time'),
-
-    rviz_launch_cmd, 
+ 
     ydlidar_launch_cmd,
     differential_drive_node,
-    camera_node
+    camera_node,
+    joint_state_publisher_node,
+    robot_state_publisher_node
 
   ]
 )
--- tortoisebot_description/launch/rviz.launch.py
+++ tortoisebot_description/launch/rviz.launch.py
@@ -9,17 +9,7 @@ def generate_launch_description():
     default_model_path = os.path.join(pkg_share, 'models/urdf/tortoisebot.xacro')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config_new.rviz')
     use_sim_time = LaunchConfiguration('use_sim_time')
-    robot_state_publisher_node = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'use_sim_time': use_sim_time},{'robot_description': ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),value_type=str)}]
-    )
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters= [{'use_sim_time': use_sim_time}],
-    )
+
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
@@ -34,13 +24,11 @@ def generate_launch_description():
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='False',
                                     description='Flag to enable use_sim_time'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                            description='Absolute path to robot urdf file'),
+        
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
 
-        joint_state_publisher_node,
-        robot_state_publisher_node,
-        rviz_node,
+        
+        rviz_node
     ])
     
