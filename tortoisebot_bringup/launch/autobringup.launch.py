2eaf82ecf914d8726e4037d2e4159fd3e4eedb07
kartik soni <kartiksoni010@gmail.com>
2023-02-12 19:08:43 +0530

    map based navigation working

--- tortoisebot_bringup/launch/autobringup.launch.py
+++ tortoisebot_bringup/launch/autobringup.launch.py
@@ -2,13 +2,16 @@ import os
 import launch
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration, PythonExpression,Command
 from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable,IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
+import launch_ros
+from launch_ros.descriptions import ParameterValue
 
 def generate_launch_description():
+  pkg_share = launch_ros.substitutions.FindPackageShare(package='tortoisebot_description').find('tortoisebot_description')
   bringup_dir = os.path.join(get_package_share_directory('tortoisebot_bringup'), 'launch')
   navigation_dir = os.path.join(get_package_share_directory('tortoisebot_navigation'), 'launch')
   rviz_launch_dir=os.path.join(get_package_share_directory('tortoisebot_description'), 'launch')
@@ -16,8 +19,10 @@ def generate_launch_description():
   ydlidar_launch_dir=os.path.join(get_package_share_directory('ydlidar'), 'launch')
   cartographer_launch_dir=os.path.join(get_package_share_directory('tortoisebot_slam'), 'launch')
   prefix_address = get_package_share_directory('tortoisebot_slam') 
+  default_model_path = os.path.join(pkg_share, 'models/urdf/tortoisebot.xacro')
   params_file= os.path.join(prefix_address, 'config', 'nav2_params.yaml')
-  map_file = os.path.join(bringup_dir, 'maps', 'room2.yaml')
+  map_file = os.path.join(get_package_share_directory(
+        'tortoisebot_bringup'), 'maps', 'room2.yaml')
   use_sim_time=LaunchConfiguration('use_sim_time')
   slam=LaunchConfiguration('slam')   
   rviz_launch_cmd=IncludeLaunchDescription(
@@ -50,15 +55,28 @@ def generate_launch_description():
   
   differential_drive_node = Node(
         package='tortoisebot_firmware',
+        condition=IfCondition(PythonExpression(['not ', use_sim_time])),
         executable='differential.py',
         name ='differential_drive_publisher',
     )
   camera_node = Node(
       package='raspicam2',
+      condition=IfCondition(PythonExpression(['not ', use_sim_time])),
       executable='raspicam2_node',
       name ='pi_camera',
     )
+  robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time},{'robot_description': ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),value_type=str)}]
+    )
 
+  joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters= [{'use_sim_time': use_sim_time}],
+    )
   return LaunchDescription([
 
     SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
@@ -66,6 +84,8 @@ def generate_launch_description():
                                             description='Flag to enable use_sim_time'),
     launch.actions.DeclareLaunchArgument(name='slam', default_value='True',
                                             description='Flag to enable use_sim_time'),
+    launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                          description='Absolute path to robot urdf file'),
     Node(
         package='nav2_map_server',
         condition=IfCondition(PythonExpression(['not ', slam])),
@@ -86,6 +106,8 @@ def generate_launch_description():
                     {'node_names': ['map_server']}]),
 
     rviz_launch_cmd,
+    robot_state_publisher_node,
+    joint_state_publisher_node,
     gazebo_launch_cmd,
     navigation_launch_cmd, 
     cartographer_launch_cmd,  
