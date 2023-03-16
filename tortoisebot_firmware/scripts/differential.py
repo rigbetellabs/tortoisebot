5384e66477b0057bfab5712893f63420923cfd81
sanket369 <skhade369@gmail.com>
2023-03-16 14:47:21 +0000

    bringup camera,lidar port,differential

--- tortoisebot_firmware/scripts/differential.py
+++ tortoisebot_firmware/scripts/differential.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Int32, Bool
 import RPi.GPIO as GPIO
 import time
 from math import pi
 
-leftEn = 13         #   Purple
-rightEn = 12        #   Red
+leftEn = 13         #   Blue
+rightEn = 12        #   Brown
 
-leftBackward = 5    #   Blue
-leftForward = 6     #   Green
-rightForward = 16   #   Yellow
-rightBackward = 20  #   Orange
+leftBackward = 5    #   Green
+leftForward = 6     #   Yellow
+rightForward = 16   #   Orange
+rightBackward = 20  #   Red
 
 motor_rpm = 60              #   max rpm of motor on full voltage 
 wheel_diameter = 0.065      #   in meters
 wheel_separation = 0.17     #   in meters
+max_pwm_val = 100           #   100 for Raspberry Pi , 255 for Arduino
+min_pwm_val = 0            #   Minimum PWM value that is needed for the robot to move
 
 wheel_radius = wheel_diameter/2
 circumference_of_wheel = 2 * pi * wheel_radius
 max_speed = (circumference_of_wheel*motor_rpm)/60   #   m/sec
+max_ang = (max_speed*2)/wheel_separation            #   rad/sec
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -35,102 +39,77 @@ pwmL = GPIO.PWM(leftEn, 100)
 pwmL.start(0)
 pwmR = GPIO.PWM(rightEn, 100)
 pwmR.start(0)
-
-def stop():
-    #print('stopping')
-    pwmL.ChangeDutyCycle(0)
-    GPIO.output(leftForward, GPIO.HIGH)
-    GPIO.output(leftBackward, GPIO.HIGH)
-    pwmR.ChangeDutyCycle(0)
-    GPIO.output(rightForward, GPIO.HIGH)
-    GPIO.output(rightBackward, GPIO.HIGH)
-
-def forward(left_speed, right_speed):
-    #print('going forward')
-    lspeedPWM = min(((left_speed/max_speed)*100),100)
-    rspeedPWM = min(((right_speed/max_speed)*100),100)
-    #print(str(left_speed)+" "+str(right_speed))
+    
+def wheel_vel_executer(left_speed, right_speed):
+    global max_pwm_val
+    global min_pwm_val
+    
+    global lpwm_pub
+    global rpwm_pub
+    global ldir_pub
+    global rdir_pub
+    
+    lspeedPWM = max(min(((abs(left_speed)/max_speed)*max_pwm_val),max_pwm_val),min_pwm_val)
+    rspeedPWM = max(min(((abs(right_speed)/max_speed)*max_pwm_val),max_pwm_val),min_pwm_val)
     pwmL.ChangeDutyCycle(lspeedPWM)
     pwmR.ChangeDutyCycle(rspeedPWM)
-    GPIO.output(leftForward, GPIO.HIGH)
-    GPIO.output(rightForward, GPIO.HIGH)
-    GPIO.output(leftBackward, GPIO.LOW)
-    GPIO.output(rightBackward, GPIO.LOW)
-
-def backward(left_speed, right_speed):
-    #print('going backward')
-    lspeedPWM = min(((left_speed/max_speed)*100),100)
-    rspeedPWM = min(((right_speed/max_speed)*100),100)
-    #print(str(left_speed)+" "+str(right_speed))
-    pwmL.ChangeDutyCycle(lspeedPWM)
-    pwmR.ChangeDutyCycle(rspeedPWM)
-    GPIO.output(leftForward, GPIO.LOW)
-    GPIO.output(rightForward, GPIO.LOW)
-    GPIO.output(leftBackward, GPIO.HIGH)
-    GPIO.output(rightBackward, GPIO.HIGH)
-
-def left(left_speed, right_speed):
-    #print('turning left')
-    lspeedPWM = min(((left_speed/max_speed)*100),100)
-    rspeedPWM = min(((right_speed/max_speed)*100),100)
-    pwmL.ChangeDutyCycle(lspeedPWM)
-    pwmR.ChangeDutyCycle(rspeedPWM)
-    GPIO.output(leftForward, GPIO.LOW)
-    GPIO.output(leftBackward, GPIO.HIGH)
-    GPIO.output(rightForward, GPIO.HIGH)
-    GPIO.output(rightBackward, GPIO.LOW)
-
-def right(left_speed, right_speed):
-    #print('turning right')
-    lspeedPWM = min(((left_speed/max_speed)*100),100)
-    rspeedPWM = min(((right_speed/max_speed)*100),100)
-    pwmL.ChangeDutyCycle(lspeedPWM)
-    pwmR.ChangeDutyCycle(rspeedPWM)
-    GPIO.output(leftForward, GPIO.HIGH)
-    GPIO.output(leftBackward, GPIO.LOW)
-    GPIO.output(rightForward, GPIO.LOW)
-    GPIO.output(rightBackward, GPIO.HIGH)
+    
+    lpwm_pub.publish(int(lspeedPWM))
+    rpwm_pub.publish(int(rspeedPWM))
+    
+    if left_speed >= 0 :
+        GPIO.output(leftForward, GPIO.HIGH)
+        GPIO.output(leftBackward, GPIO.LOW)
+        ldir_pub.publish(1)
+    else :
+        GPIO.output(leftForward, GPIO.LOW)
+        GPIO.output(leftBackward, GPIO.HIGH)
+        ldir_pub.publish(0)
+        
+    if right_speed >= 0 :
+        GPIO.output(rightForward, GPIO.HIGH)
+        GPIO.output(rightBackward, GPIO.LOW)
+        rdir_pub.publish(1)
+    else :
+        GPIO.output(rightForward, GPIO.LOW)
+        GPIO.output(rightBackward, GPIO.HIGH)
+        rdir_pub.publish(0)
     
 def callback(data):
+
+    # refer this for understanding the formula 
+    # http://www.cs.columbia.edu/~allen/F17/NOTES/icckinematics.pdf
     
     global wheel_radius
     global wheel_separation
     
-    linear_vel = data.linear.x
-    angular_vel = data.angular.z
+    linear_vel = data.linear.x                  # Linear Velocity of Robot
+    angular_vel = data.angular.z                # Angular Velocity of Robot
     #print(str(linear)+"\t"+str(angular))
     
-    rplusl  = ( 2 * linear_vel ) / wheel_radius
-    rminusl = ( angular_vel * wheel_separation ) / wheel_radius
+    VrplusVl  = 2 * linear_vel
+    VrminusVl = angular_vel * wheel_separation
     
-    right_omega = ( rplusl + rminusl ) / 2
-    left_omega  = rplusl - right_omega 
-    
-    right_vel = right_omega * wheel_radius
-    left_vel  = left_omega  * wheel_radius
+    right_vel = ( VrplusVl + VrminusVl ) / 2      # right wheel velocity along the ground
+    left_vel  = VrplusVl - right_vel              # left wheel velocity along the ground
     
     #print (str(left_vel)+"\t"+str(right_vel))
-    '''
-    left_speed  = abs ( linear - ( (wheel_separation/2) * (angular) ) )
-    right_speed = abs ( linear - ( (wheel_separation/2) * (angular) ) )
-    '''
     
-    if (left_vel == 0.0 and right_vel == 0.0):
-        stop()
-    elif (left_vel >= 0.0 and right_vel >= 0.0):
-        forward(abs(left_vel), abs(right_vel))
-    elif (left_vel <= 0.0 and right_vel <= 0.0):
-        backward(abs(left_vel), abs(right_vel))
-    elif (left_vel < 0.0 and right_vel > 0.0):
-        left(abs(left_vel), abs(right_vel))
-    elif (left_vel > 0.0 and right_vel < 0.0):
-        right(abs(left_vel), abs(right_vel))
-    else:
-        stop()
+    wheel_vel_executer(left_vel, right_vel)
         
 def listener():
+    
+    global lpwm_pub
+    global rpwm_pub
+    global ldir_pub
+    global rdir_pub
+    
     rospy.init_node('cmdvel_listener', anonymous=False)
     rospy.Subscriber("/cmd_vel", Twist, callback)
+    lpwm_pub = rospy.Publisher('lpwm', Int32, queue_size = 10)
+    rpwm_pub = rospy.Publisher('rpwm', Int32, queue_size = 10)
+    ldir_pub = rospy.Publisher('ldir', Bool, queue_size = 10)
+    rdir_pub = rospy.Publisher('rdir', Bool, queue_size = 10)
     rospy.spin()
 
 if __name__== '__main__':
@@ -139,4 +118,5 @@ if __name__== '__main__':
     print('Wheel Diameter:\t'+str(wheel_diameter)+' m')
     print('Wheel Separation:\t'+str(wheel_separation)+' m')
     print('Robot Max Speed:\t'+str(max_speed)+' m/sec')
+    print('Max Angular Speed:\t'+str(max_ang)+' rad/sec')
     listener()
