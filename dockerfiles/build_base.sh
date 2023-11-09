9bf39b568f14c11e38ec245cb61b4fa32dd7500f
Aaryan Murgunde (Spectre Jammy) <aaryan.murgunde@gmail.com>
2023-11-09 16:51:03 +0530

    new fs
    Signed-off-by: Aaryan Murgunde (Spectre Jammy) <aaryan.murgunde@gmail.com>

---
+++ dockerfiles/build_base.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+docker build -t ttb:base -f dockerfiles/Dockerfile.base . && \
+echo -e "\e[92mRunning docker container now ...\e[0m" && \
+docker run --device=/dev/dri \
+			-it \
+			--privileged \
+			-e "DISPLAY=unix:0.0" \
+			-v="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+			--network host \
+			--rm \
+			--name ttb_testing \
+			ttb:base
