c2c985e82d9d3339b50d63c2f712dfd3763a1816
Raunak abhani <68866046+Raunakabhani@users.noreply.github.com>
2023-12-02 16:43:58 +0530

    Delete raspicam2_node directory

--- raspicam2_node/src/raspicam2_node.cpp
+++
@@ -1,8 +0,0 @@
-#include <RasPiCamPublisherNode.hpp>
-
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<RasPiCamPublisher>());
-    rclcpp::shutdown();
-    return 0;
-}
